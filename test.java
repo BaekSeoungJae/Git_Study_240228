4eb3a800b7b06a30ddf63ff32332ebcbcb9f0c25
goyang1103 <00bsj@naver.com>
2024-09-11 15:14:47 +0900

    git test

--- test.java
+++ test.java
@@ -3,9 +3,6 @@ String git2 = "test2";
 String git3 = "test3";
 String git4 = "test4";
 String git5 = "test5";
-<<<<<<< HEAD
-String git6 = "test6";
-=======
 String git6 = "test6";
 String git7 = "test7";
->>>>>>> 1b6f7459e12bad55f2041b43101789e05f9efbfd
+String git8 = "test8";
